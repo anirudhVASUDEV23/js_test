abb59296fc84e40fe00ca3499fd9421eb8bb9761
anirudhVASUDEV23 <122335901+anirudhVASUDEV23@users.noreply.github.com>
2024-12-08 05:20:45 +0000

    config done

---
+++ 01_basics/variable.js
@@ -0,0 +1,18 @@
+const accountId=1236554
+let accountemail="xyz@gmail.com"
+var accountPassword="1222345"
+accountCity="Jaipur"
+let accountstate;
+
+//accountId=7843398 not allowed
+
+accountemail="anirudh@gmail.com"
+accountPassword="463667"
+accountCity="Bangalore"
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId,accountemail,accountPassword,accountCity,accountstate])
