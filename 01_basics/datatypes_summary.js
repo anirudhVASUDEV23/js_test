aa9090fc7e795ea6de8c7ea28d175501a0ca41dd
anirudhVASUDEV23 <122335901+anirudhVASUDEV23@users.noreply.github.com>
2024-12-08 06:13:29 +0000

    changes done

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -35,3 +35,42 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+//Stack(Primitive) v/s heap(refernce)
+
+let myname="Anirudh"
+let another_name=myname
+
+console.log("Printing before for primitive");
+console.log(myname);
+console.log(another_name);
+
+another_name="Vasudev"
+console.log("Printing after for primitive");
+console.log(myname);
+console.log(another_name);
+
+let userOne={
+    email:"userone@gmail.com",
+    upi:"12345"
+}
+
+let userTwo=userOne
+
+console.log("Printing before for reference");
+console.log(userOne.upi);
+console.log(userTwo.upi);
+
+userTwo.upi="54321"
+
+console.log("Printing after for reference");
+console.log(userOne.upi);
+console.log(userTwo.upi);
+
+
+
+
+
+
+
